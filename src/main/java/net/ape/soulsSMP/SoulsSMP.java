f154b9efb871cfb2214781a04eafa818475dd702
ApeTakeOver <sithayarajah@gmail.com>
2026-07-25 01:42:50 -0400

    First Commit - Souls SMP fuh you pumpkin this shit bouta be easy

---
+++ src/main/java/net/ape/soulsSMP/SoulsSMP.java
@@ -0,0 +1,16 @@
+package net.ape.soulsSMP;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SoulsSMP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
